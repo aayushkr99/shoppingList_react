1e0187fd7537455cee6dc7ec088c4c9ac7a7486c
aayushkr99 <ak858098@gmail.com>
2023-02-15 09:05:28 +0530

    some css applied

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Button, Stack, TextField, Typography } from "@mui/material";
 
 function App() {
+  const [newItem, setNewItem] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newItems = e.target.item.value;
+    setNewItem([...newItem, newItems]);
+    e.target.reset();
+  };
+
+  const removeItem = (item) => {
+    const newItems = newItem.filter((items) => items !== item);
+    setNewItem(newItems);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>SHOPPING LIST</h1>
+        <div>
+          <Stack marginBottom={8}>
+            <Typography variant="h4" color="brown">
+              Item List
+            </Typography>
+          </Stack>
+          <Stack spacing={2}>
+            <form onSubmit={handleSubmit}>
+              <TextField type="text" name="item" label="add new item" />
+              <Button type="submit" variant="outlined">
+                add
+              </Button>
+            </form>
+          </Stack>
+          <Stack>
+            <ul>
+              {newItem.map((item, index) => (
+                <Item item={item} key={item + index} removeItem={removeItem} />
+              ))}
+            </ul>
+          </Stack>
+        </div>
+      </div>
     </div>
+  );
+}
+function Item(props) {
+  return (
+    <Stack display="flex">
+      <Typography variant="h5" color="brown">
+        {props.item}
+      </Typography>
+      <Button variant="text" onClick={() => props.removeItem(props.item)}>
+        remove
+      </Button>
+    </Stack>
   );
 }
 
